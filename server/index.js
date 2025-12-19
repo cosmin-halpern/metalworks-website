4cb756ed43fe60015d23de9fec4d2ca86519a0f5
cosmin-halpern <145839120+cosmin-halpern@users.noreply.github.com>
2025-12-19 17:17:11 +0200

    Fix: Add missing newline at the end of index.js

--- server/index.js
+++ server/index.js
@@ -2,4 +2,5 @@
 // Use the absolute path to your home directory
 import('/home/corsican/repositories/metalworks-website/server/dist/index.js').catch(err => {
     console.error('Startup Error:', err);
+
 });
